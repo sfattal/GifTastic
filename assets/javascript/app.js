3139867e6085688b743cb49f8ecbdf02f6cc87e0
sfattal <fattal.sherif@gmail.com>
2018-10-31 18:17:09 -0400

    ajax function IP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Buttons:
 var topics = ["awkward", "bored", "confused", "angry", "excited", "drunk", "frustrated", "hungry", "mind-blown", "tired", "disappointed", "embarrassed", "relaxed", "happy", "sad", "stressed", "nervous"];
 
 for (i = 0; i < topics.length; i++) {
@@ -5,3 +6,33 @@ for (i = 0; i < topics.length; i++) {
     buttons.text(topics[i])
     $("#buttons").append(buttons);
 }
+
+$("buttons").on("click", function() {
+    event.preventDefault();
+    var emotion = $(this).attr(topics[i]);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=OAOml4nsVDKZnPwbip5iGgvQf4CSMOoO&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+        var results = response;
+
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+
+            var emotionImage = $("<img>");
+            emotionImage.attr("src", results[i].images.fixed_height.url);
+
+            gifDiv.append(p);
+            gifDiv.append(emotionImage);
+
+            $("#output").append(gifDiv);
+        }
+        console.log(results)
+    });
+})
