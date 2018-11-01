4fdab2fdaaebe1725105e45d9bb65c2848af8a99
sfattal <fattal.sherif@gmail.com>
2018-10-31 20:27:41 -0400

    gifs load in still state and can pause/unpause

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,22 +12,40 @@ for (i = 0; i < topics.length; i++) {
 $(document).on("click", ".btns", function() {
     var emotion = $(this).val();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=OAOml4nsVDKZnPwbip5iGgvQf4CSMOoO&limit=10";
-
+    
     $.ajax({
         url: queryURL,
         method: "GET"
     })
       .then(function(response) {
         var results = response.data;
+        console.log(response)
         for (var i = 0; i < results.length; i++) {
             var gifDiv = $("<div>");          
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
             var emotionImage = $("<img>");
-            emotionImage.attr("src", results[i].images.fixed_height.url);
+            emotionImage.attr({
+                "src": results[i].images.downsized_still.url, 
+                "data-still": results[i].images.downsized_still.url, 
+                "data-animate": results[i].images.downsized.url,
+                "data-state": "still"
+            });
+            emotionImage.addClass("imgs");
             gifDiv.append(p);
             gifDiv.append(emotionImage);
             $("#output").prepend(gifDiv);
         }
     });
 })
+
+$(document).on("click", ".imgs", function() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+    }
+});
